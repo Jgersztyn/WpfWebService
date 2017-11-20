d5dde44f60bccd458c324c49bda7ef7fcad3b14b
Jason Gersztyn <jgersztyn91@gmail.com>
2017-11-19 19:59:39 -0800

    Intitial commit with basic app structure.

---
+++ WpfWebService/CreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WpfWebService
+{
+    class CreateRequest
+    {
+        private DataHandler handler;
+
+        public CreateRequest()
+        {
+            handler = new DataHandler();
+        }
+    }
+}
