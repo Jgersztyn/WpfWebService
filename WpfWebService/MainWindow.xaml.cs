dba3736fea8725d3c4c5aad161746b663e0a6293
Jason Gersztyn <jgersztyn91@gmail.com>
2017-11-26 22:48:06 -0800

    Adding some basic functionality for the web client.

--- WpfWebService/MainWindow.xaml.cs
+++ WpfWebService/MainWindow.xaml.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Windows;
+
 namespace WpfWebService
 {
     public partial class MainWindow : Window
     {
+        private CreateRequest request = new CreateRequest();
+        private string connection = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Display the results using this action
         }
     }
 }
